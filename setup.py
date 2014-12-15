a0375e33f58d3f94559de8e50e51e075eaae0dfe
brooksc <brooksc@brooksc.com>
2014-12-14 20:16:21 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'bugherd',
+  packages = ['bugherd'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Access bugherd.com API',
+  author = 'Brooks Cutter',
+  author_email = 'brooksc@brooksc.com',
+  url = 'https://github.com/brooksc/bugherd', # use the URL to the github repo
+  download_url = 'https://github.com/brooksc/bugherd/tarball/0.1', # I'll explain this in a second
+  keywords = ['bugherd'], # arbitrary keywords
+  classifiers = [],
+)
